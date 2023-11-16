6cfc89df0c2f0e3be73e5bba59c391e45743de3b
Javier André Hernández Hernández <andrehdz@MacBook-Pro-de-Javier-2.local>
2023-11-16 12:21:06 -0600

    agregar funcion enviar correo

--- public/app.js
+++ public/app.js
@@ -97,3 +97,69 @@ var swiper = new Swiper(".mySwiper", {
       },
     },
   });
+
+  window.onload = function () {
+    document.getElementById("captcha").innerHTML = "";
+    var charsArray =
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
+    var lengthOtp = 6;
+    var captcha = [];
+    for (var i = 0; i < lengthOtp; i++) {
+        //below code will not allow Repetition of Characters
+        var index = Math.floor(Math.random() * charsArray.length + 1); //get the next character from the array
+        if (captcha.indexOf(charsArray[index]) == -1)
+            captcha.push(charsArray[index]);
+        else i--;
+    }
+    var canv = document.createElement("canvas");
+    canv.id = "captcha";
+    canv.width = 100;
+    canv.height = 50;
+    var ctx = canv.getContext("2d");
+    ctx.font = "25px Georgia";
+    ctx.strokeText(captcha.join(""), 0, 30);
+    //storing captcha so that can validate you can save it somewhere else according to your specific requirements
+    code = captcha.join("");
+    document.getElementById("captcha").appendChild(canv); // adds the canvas to the body element
+    const box = document.querySelector('#box');
+    const pElement = document.createElement('p');
+    pElement.classList.add('text-white', 'p-2', 'text-center', 'rounded-md', 'mt-5' , 'py-3');
+    document
+          .getElementById("contact-form")
+          .addEventListener("submit", function (event) {
+              event.preventDefault();
+
+  
+              if (document.getElementById("cpatchaTextBox").value == code) {
+                  
+                pElement.textContent = 'Enviando...';
+                  pElement.classList.add('green');
+                  box.appendChild(pElement);
+
+                  emailjs
+                      .sendForm("default_service", "template_8mfzbdt", this)
+                      .then(
+                          function () {
+                              pElement.textContent = 'Correo Enviado';
+                              document.getElementById("contact-form").reset();
+                              setTimeout(function() {
+                                  box.removeChild(pElement);
+                                  pElement.classList.remove('green');
+                              }, 5000);
+                          },
+                          function (error) {
+                              console.log("FAILED...", error);
+                          }
+                      );
+              } else {
+                  pElement.textContent = 'Captcha Incorrecto';
+                  pElement.classList.add('red');
+                  box.appendChild(pElement);
+                  setTimeout(function() {
+                      box.removeChild(pElement);
+                      pElement.classList.remove('red');
+                  }, 3000);
+                  
+              }
+          });
+};
